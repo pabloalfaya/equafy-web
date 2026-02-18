7c9e688c89fba144c7e0e701955f6ed602f0bd44
Pablo Alfaya <tu_email@ejemplo.com>
2026-02-18 17:29:53 +0100

    feat: updated stripe session logic via code

--- src/app/api/stripe/checkout/route.ts
+++ src/app/api/stripe/checkout/route.ts
@@ -116,6 +116,7 @@ export async function POST(req: Request) {
         userEmail: userEmail,
       },
       subscription_data: {
+        trial_period_days: 14,
         metadata: {
           projectId: projectId,
           userEmail: userEmail,
